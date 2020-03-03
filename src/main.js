6c4d5812dfa6c66f7bc231846787188d60c6ef58
wangge <1727084024.qq.com>
2020-03-03 13:32:19 +0800

    黑马头条PC-初始化项目-整理目录-准白资源

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './style/index' // 去除浏览器默认样式css文件
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
