0b6e7e39bc793128733e8504cfbad9b55def7baf
wangge <1727084024.qq.com>
2020-03-31 10:23:47 +0800

    安装axios 配置全局使用

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // import './style/index.css' // 去除浏览器默认样式css文件
 import './style/clear.css' // 清除浏览器默认边距
+import axios from 'axios'
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios // 让所有的vue实例都可以使用axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 请求基地址
 Vue.use(ElementUI)
 new Vue({
   router,
