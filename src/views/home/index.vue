4ec9fbc874bb06790cd1cca7add64660d8ca015d
wangge <1727084024.qq.com>
2020-04-03 10:51:01 +0800

    主页 布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
-    首页
+    <!-- 布局-->
+    <el-container>
+      <!-- 左侧边栏容器 -->
+      <el-aside></el-aside>
+      <!-- 右侧大容器 -->
+      <el-container>
+        <!-- 顶栏容器 -->
+        <el-header></el-header>
+        <!-- 主要区域容器 -->
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
